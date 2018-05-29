1f1229cc1f4bde4a3e9217794e8274b4fb849ab2
Andy <ajlewis1@me.com>
2018-05-29 13:13:39 -0600

    Initial Commit

---
+++ PhotoSorter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PhotoSorter
+//
+//  Created by Andrew Lewis on 5/29/18.
+//  Copyright © 2018 Andrew Lewis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
